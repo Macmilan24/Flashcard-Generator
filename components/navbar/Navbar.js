498d271c273972c0a8b35d78d6c862b6ec2e3da9
Macmilan24 <cpt.macmilan24@gmail.com>
2024-08-17 09:44:29 +0300

    updated the sign in btn

--- components/navbar/Navbar.js
+++ components/navbar/Navbar.js
@@ -154,7 +154,7 @@ export default function Navbar() {
             </a>
           </Link>
           <SignedOut>
-            <div className="hidden lg:flex space-x-2 sm:space-x-4 mt-2 sm:mt-0">
+            <div className=" flex space-x-2 sm:space-x-4 mt-2 sm:mt-0">
               <motion.a
                 className="bg-white text-black rounded-full px-4 sm:px-5 py-2 font-semibold relative overflow-hidden group"
                 whileHover={{ backgroundColor: "#000", color: "#fff" }}
@@ -184,7 +184,7 @@ export default function Navbar() {
             </div>
           </SignedOut>
           <SignedIn>
-            <div className="hidden lg:flex space-x-2 sm:space-x-4 mt-2 sm:mt-0">
+            <div className="flex space-x-2 sm:space-x-4 mt-2 sm:mt-0">
               <UserButton />
             </div>
           </SignedIn>
